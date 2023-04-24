13be14f8f7e6bd4fefb5816e822d04af936774cd
rockfornik <nekmokshan@mail.ru>
2023-04-24 17:56:59 +0300

    for, while, do while [#3]

--- src/Cycle.java
+++ src/Cycle.java
@@ -7,15 +7,17 @@ public class Cycle {
         System.out.print("Введите 100 или 10: ");
         first2 = num2.nextInt();
         if (first2 == 100) {
-            for (first2 = 100; first2 > 10; first2 -= 10)
+            for (first2 = 100; first2 >= 10; first2 -= 10)
                 System.out.println("Вывод чисел от 100 до 10: " + first2);
         } else if (first2 == 10) {
             while (first2 <= 100) {
                 System.out.println("Вывод чисел от 10 до 100: " + first2);
-                first2 +=10;
+                first2 += 10;
             }
+        } else {
+            do {
+                System.out.println("Вы ввели число отличное от 100 или 10: " + first2);
+            } while (first2 == 10 && first2 == 100);
         }
-        else
-            System.out.println("Вы ввели число отличное от 100 или 10");
     }
 }
