eb31f19087225fe278aa2643263093cd49e1858e
rockfornik <nekmokshan@mail.ru>
2023-04-24 16:09:56 +0300

    switch case [#2]

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,29 @@ public class Main {
         int first, second, result;
         System.out.print("Введите целое число больше 0, но меньше 100: ");
         first = num.nextInt();
-        if (first < 0)
+        if (first < 0) {
             System.out.println("Вы ввели число меньше 0");
-        else if (first > 100)
+        } else if (first > 100) {
             System.out.println("Вы ввели число больше 100");
-        else {
-        System.out.print("Введите следующее целое число: ");
-        second = num.nextInt();
-        result = first+second;
-        System.out.println("сумма = "+result);
+        } else {
+            System.out.print("Введите следующее целое число: ");
+            second = num.nextInt();
+            result = first + second;
+            System.out.println("сумма = " + result);
+
+            switch (result) {
+                case 10:
+                    System.out.println("Поздравляю, сумма чисел равна десяти");
+                    break;
+                case 100:
+                    System.out.println("Поздравляю, сумма чисел равная стам");
+                    break;
+                case 1000:
+                    System.out.println("Поздравляю, сумма чисел равна тысячи");
+                    break;
+                default:
+                    System.out.println("К сожелению сумма чисел не равна: 10, 100, 1000");
+            }
         }
     }
 }
