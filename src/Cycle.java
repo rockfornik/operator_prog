f72b8b5ec2e006e5469ce53d0a09c87e227a4ad8
rockfornik <nekmokshan@mail.ru>
2023-04-24 17:40:53 +0300

    for while [#3]

--- src/Cycle.java
+++ src/Cycle.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Cycle {
     public static void main(String[] args) {
-
+        Scanner num2 = new Scanner(System.in);
+        int first2;
+        System.out.print("Введите 100 или 10: ");
+        first2 = num2.nextInt();
+        if (first2 == 100) {
+            for (first2 = 100; first2 > 10; first2 -= 10)
+                System.out.println("Вывод чисел от 100 до 10: " + first2);
+        } else if (first2 == 10) {
+            while (first2 <= 100) {
+                System.out.println("Вывод чисел от 10 до 100: " + first2);
+                first2 +=10;
+            }
+        }
+        else
+            System.out.println("Вы ввели число отличное от 100 или 10");
     }
 }
