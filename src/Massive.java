23d7356aff9e5bda7fdfa7a897b88b80650f86fe
rockfornik <nekmokshan@mail.ru>
2023-05-03 16:32:18 +0300

    Массивы [#4]

---
+++ src/Massive.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Massive {
+    public static void main(String[] args) {
+        int n;
+        int [] a;
+        int sum = 0;
+        Scanner i = new Scanner(System.in);
+        System.out.print("Введите положительное количество эллементов массива: ");
+        n = i.nextInt();
+        a = new int[n];
+        for (int z = 0; z < n; z++){
+        System.out.print("Введите a[" + z + "] = ");
+        a[z] = i.nextInt();
+        sum = sum + a[z];
+        }
+        System.out.print("Сумма элементов массива = " + sum);
+    }
+}
