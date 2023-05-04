8ef305e50fa743fcacce4ec5f509cad3cbb25e42
rockfornik <nekmokshan@mail.ru>
2023-05-04 16:32:56 +0300

    Функции [#5]

---
+++ src/Func.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Func {
+    public static void main(String[] args) {
+        Scanner one = new Scanner(System.in);
+        int a, b;
+        System.out.print("Введите число: ");
+        a = one.nextInt();
+        System.out.print("Введите положительную степень: ");
+        b = one.nextInt();
+        int temp = func (a, b);
+        System.out.print(temp);
+    }
+
+    public static int func (int a, int b) {
+        int r = 1;
+        //возведение в степень
+        for (int i = 0; i < b; i++)
+            r *= a;
+        return  r;
+    }
+}
